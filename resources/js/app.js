057265c2fa812797161015dd1bb0285b09c32340
nichiyoo <61191577+nichiyoo@users.noreply.github.com>
2026-05-16 13:22:26 +0700

    chore: add flag icon dependency to display country flag

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import 'flowbite';
 import alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 import { createIcons, icons } from 'lucide';
+import 'flag-icons/css/flag-icons.min.css';
 
 window.Alpine = alpine;
 alpine.plugin(focus);
