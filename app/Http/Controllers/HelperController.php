d8721883ccc13a70dbb2278872be41c79b34170f
Nichi <61191577+nichiyoo@users.noreply.github.com>
2026-06-18 12:26:10 +0700

    fix: change return type of root method in helper controller

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\View\View;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\RedirectResponse;
 
 class HelperController extends Controller
 {
     /**
      * Redirect default path to dashboard.
      */
-    public function root(Request $request): Redirect
+    public function root(Request $request): RedirectResponse
     {
         return redirect()->route('dashboard');
     }
